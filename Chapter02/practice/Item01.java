929d20f0591f1621f7dd85c4b50b763b562e6e96
chulhee23 <chulhee2351@gmail.com>
2021-10-24 22:35:10 +0900

    deleted

--- Chapter02/practice/Item01.java
+++
@@ -1,8 +0,0 @@
-package Chapter02.practice;
-
-public class Item01 {
-    public static void main(String[] args) {
-        Laptop laptop = Laptop.ofModelNameAndCompany("macbook Pro 16", "Apple");
-        System.out.println("laptop = " + laptop);
-    }
-}
